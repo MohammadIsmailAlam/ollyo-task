38d0c87a0aa2669f647ef0f68631250cbb4c2141
Mohammad Ismail Alam <ismailalam901@gmail.com>
2023-11-01 19:51:17 +0600

    checkbox validation added

--- src/Component/ImageItem.js
+++ src/Component/ImageItem.js
@@ -23,7 +23,9 @@ function ImageItem({ image, isSelected, onImageSelect, onReorder }) {
 
   return (
     <div
-      className={`relative border-2 border-gray-300 group md:flex items-center justify-center`}
+      className={`relative border-2 border-gray-300 group md:flex items-center justify-center ${
+        isSelected ? "border-blue-500" : isHovered ? "border-green-500" : ""
+      }`}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       draggable="true"
@@ -31,14 +33,14 @@ function ImageItem({ image, isSelected, onImageSelect, onReorder }) {
       onDragOver={handleDragOver}
       onDrop={handleDrop}
     >
-      <div className="img">
+      {(isHovered || isSelected) && (
         <input
           type="checkbox"
           className="md:absolute top-2 left-2 z-10 mt-8 ml-5 sm:w-5 sm:h-5"
           checked={isSelected}
           onChange={handleCheckboxChange}
         />
-      </div>
+      )}
       <img
         src={image.url}
         alt={`img ${image.id}`}
