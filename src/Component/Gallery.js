1941ad5c745e19e4c1cb95156810af5efcf61dbf
Mohammad Ismail Alam <ismailalam901@gmail.com>
2023-11-01 11:05:27 +0600

    add img

--- src/Component/Gallery.js
+++ src/Component/Gallery.js
@@ -1,11 +1,30 @@
-// Gallery.js
 import React from "react";
 import ImageItem from "./ImageItem";
+import AddImage from "./AddImage";
 
-function Gallery({ images, selectedImages, handleImageSelect, handleReorder, setIsDeleteButtonVisible }) {
+function Gallery({
+  images,
+  selectedImages,
+  handleImageSelect,
+  handleReorder,
+  setIsDeleteButtonVisible,
+  setImages,
+}) {
+  const handleUploadImage = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const newImage = {
+        id: images.length + 1,
+        url: URL.createObjectURL(file),
+        isFeatured: true,
+      };
+      setImages([...images, newImage]);
+    }
+  }
+
   return (
-    <div className="gallery md:grid grid-rows-3 grid-flow-col gap-4 mx-8">
-      {images.map((image) => (
+    <div className="gallery md:grid grid-cols-4 gap-4 mx-8">
+      {images.map((image, index) => (
         <ImageItem
           key={image.id}
           image={image}
@@ -13,8 +32,11 @@ function Gallery({ images, selectedImages, handleImageSelect, handleReorder, set
           onImageSelect={handleImageSelect}
           onReorder={handleReorder}
           setIsDeleteButtonVisible={setIsDeleteButtonVisible}
+          className={index === 0 ? "row-span-2 col-span-1" : "col-span-1"}
         />
       ))}
+
+      <AddImage handleUploadImage={handleUploadImage} />
     </div>
   );
 }
